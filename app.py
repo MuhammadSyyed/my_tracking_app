3b73c255371b60c8e2aef2faf7ec7e84317a650a
sm_imranabbas <imran.abbas@istoreisend.com>
2024-05-29 21:28:20 +0500

    Add routes

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__,static_folder='./static',template_folder='./templates')
 
-@app.route("/")
-def home():
-    return render_template('index.html',name="imran")
+@app.get("/<name>")
+def home(name):
+    return render_template('index.html',name=name)
+
+@app.post("/square")
+def square():
+    payload = request.get_json()
+    sqr = payload["number"]**2
+    return {"square":sqr}
 
 if __name__ == "__main__":
     app.run()
