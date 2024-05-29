745aed0801cabfe84d2f8821c08ccae3005e2ace
sm_imranabbas <imran.abbas@istoreisend.com>
2024-05-30 00:11:46 +0500

    Map can be made from ui now

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, jsonify
+from script import get_locations, generate_map_string
+from flask_cors import CORS
 
-app = Flask(__name__,static_folder='./static',template_folder='./templates')
+app = Flask(__name__, static_folder="./static", template_folder="./templates")
+CORS(app=app)
 
-@app.get("/<name>")
-def home(name):
-    return render_template('index.html',name=name)
+@app.get("/")
+def home():
+    _, locations = get_locations("csv_data/*.csv")
+    return render_template("main.html", locations=locations["desc"].values.tolist())
 
-@app.post("/square")
-def square():
-    payload = request.get_json()
-    sqr = payload["number"]**2
-    return {"square":sqr}
+
+@app.post("/get_map")
+def get_map():
+    data = request.get_json()
+    mymap = generate_map_string(data["current"],data["destination"])
+    return jsonify({"map":mymap})
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- script.py
+++ script.py
@@ -4,6 +4,7 @@ from glob import glob
 import folium
 from datetime import datetime
 from geopy.geocoders import Nominatim
+import traceback
 
 
 def get_lat_lon(location_name):
@@ -14,7 +15,8 @@ def get_lat_lon(location_name):
         return latitude, longitude
     else:
         return None
-     
+
+
 def generate_map(coordinates, location_name):
     if coordinates:
         lat = coordinates[0]
@@ -44,80 +46,82 @@ def generate_map(coordinates, location_name):
             f"Location not found for {location_name}. Please check the spelling or provide more details."
         )
 
+
 def fetch_tracking_records(df, start_location, end_location):
-    start_location_df = df[df['desc'] == start_location]
-    end_location_df = df[df['desc'] == end_location]
+    start_location_df = df[df["desc"] == start_location]
+    end_location_df = df[df["desc"] == end_location]
     if start_location_df.empty or end_location_df.empty:
         print(f"One or both of the specified locations not found.")
         return pd.DataFrame()
     start_index = start_location_df.index[0]
     end_index = end_location_df.index[0]
-    tracking_records = df.loc[start_index:end_index, ['latitude', 'longitude']]
+    tracking_records = df.loc[start_index:end_index, ["latitude", "longitude"]]
     return tracking_records
 
+
 def gather_data(path_to_files_folder):
-    '''This function gathers all files and generate a single file'''
+    """This function gathers all files and generate a single file"""
     files = glob(path_to_files_folder)
     final = pd.DataFrame()
     for file in files:
         file_data = pd.read_csv(files[0])
-        final = pd.concat([final,file_data])
+        final = pd.concat([final, file_data])
     data = final.copy(deep=True)
     data = data.drop_duplicates()
-    data = data[['type','latitude','longitude','desc']]
+    data = data[["type", "latitude", "longitude", "desc"]]
+    return data
 
-def generate_tracking_memory(reference,tracking):
+
+def generate_tracking_memory(reference, tracking):
     tracking_memory = {}
-    all_locations = list(reference['desc'])
+    all_locations = list(reference["desc"])
     for i, fromm in enumerate(all_locations):
         for j, tto in enumerate(all_locations):
             if i != j:  # Ensure different locations
-                key = f'{fromm}-{tto}'
-                reverse_key = f'{tto}-{fromm}'
+                key = f"{fromm}-{tto}"
+                reverse_key = f"{tto}-{fromm}"
                 value = fetch_tracking_records(tracking, fromm, tto)
                 if not value.empty:
                     tracking_memory[key] = value
                     tracking_memory[reverse_key] = value
     return tracking_memory
 
-def label_data(reference,tracking):
-    for lat,long,label in zip(reference.latitude,reference.longitude,reference.desc):
-        condition = (tracking['latitude']==lat)&(tracking['longitude']==long)
-        print("Marking",label)
-        tracking.loc[condition, 'desc'] = label
+
+def label_data(reference, tracking):
+    for lat, long, label in zip(
+        reference.latitude, reference.longitude, reference.desc
+    ):
+        condition = (tracking["latitude"] == lat) & (tracking["longitude"] == long)
+        print("Marking", label)
+        tracking.loc[condition, "desc"] = label
     return tracking
 
 
-if __name__ == "__main__":
-    
-    try:
+def get_locations(files_folder):
+    data = gather_data(files_folder)
+    reference = data[data["type"] == "W"]
+    return data, reference
 
-        data = gather_data('./csv_data/*.csv')
 
-        reference = data[data['type'] =='W']
+def generate_map_string(current, destination):
+    data, reference = get_locations("csv_data/*.csv")
+    reference = data[data["type"] == "W"]
+    tracking = data[data["type"] == "T"]
+    tracking = label_data(reference, tracking)
+    tracking_memory = generate_tracking_memory(reference, tracking)
+    lat, long = get_lat_lon("Sindh Madressa-tul-Islam University")
+    smiu_map = folium.Map(location=[lat, long], zoom_start=150)
+    for lat, lng, label in zip(reference.latitude, reference.longitude, reference.desc):
+        folium.Marker(
+            [lat, lng],
+            popup=label,
+        ).add_to(smiu_map)
 
-        tracking = data[data['type'] =='T']
-
-        tracking = label_data(reference,tracking)
-
-        tracking_memory = generate_tracking_memory(reference,tracking)
-
-        lat,long = get_lat_lon('Sindh Madressa-tul-Islam University')
-
-        smiu_map = folium.Map(location=[lat, long], zoom_start=150)
-
-        for lat, lng, label in zip(reference.latitude, reference.longitude, reference.desc):
-            folium.Marker(
-                [lat, lng],
-                popup=label,
-            ).add_to(smiu_map)
-
-        track = tracking_memory['Parking-Talpur house']
-
-        points = [(x, y) for x, y in zip(track.latitude, track.longitude)]
-        folium.PolyLine(points, color="yellow", weight=5, opacity=0.85).add_to(smiu_map)
-
-        smiu_map.save("output.html")
-
-    except Exception as e:
-        print(e)
+    track = tracking_memory[f"{current}-{destination}"]
+    points = [(x, y) for x, y in zip(track.latitude, track.longitude)]
+    folium.PolyLine(points, color="yellow", weight=5, opacity=0.85).add_to(smiu_map)
+    filename = f"./maps/{current}-{destination}.html"
+    smiu_map.save(filename)
+    with open(filename, 'r') as file:
+        html_content = file.read()
+        return html_content
